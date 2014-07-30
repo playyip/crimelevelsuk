41cc1144e93dd65d4faacfe885f4300d5796c6da
Maxamations <max.ohegarty@gmail.com>
2014-07-30 15:01:06 +0100

    Latest js changes

--- htdox/crimespi.js
+++ htdox/crimespi.js
@@ -1,3 +1,12 @@
+var crimes = {};
+var colours = ['#00FF00','#00FFFF','#008000','#B22222','#0000CD','#FF4500','#6B8E23','#000080','#00FF7F','#4682B4','#FF6347','#800000','#008080','#32CD32','#008080']
+
+var colourPos = 0;
+function getNextColour() {
+    var myColour = colours[colourPos];
+    colourPos ++;
+    return myColour;
+}
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -11,11 +20,9 @@ function showPosition(position) {
 }
 
 function processApi(allText, lat, lng) {
-
+    
   var show_image = true;
   var images = makeImages();
-  var crimes = crimeCounter();
-  
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: new google.maps.LatLng(lat, lng),
@@ -28,11 +35,7 @@ function processApi(allText, lat, lng) {
 
       var counter = 0;
       Object.keys(images).forEach(function (crimeName) {
-          if (crimes[crimeName] != null) {
-              counter++;
-              crimes[crimeName] = crimes[crimeName] += 1;
-              alert(crimes['burglary']);
-          }
+
           var icon = images[crimeName];
           var div = document.createElement('div');
           div.innerHTML = '<img src="' + icon + '"> ' + crimeName;
@@ -43,7 +46,7 @@ function processApi(allText, lat, lng) {
   var infowindow = new google.maps.InfoWindow();
   var marker, i;
   for (var i=0; i<jsonArray.length; i++) {
-
+   addCrime(jsonArray[i].category)
    marker = new google.maps.Marker({
       position: new google.maps.LatLng(jsonArray[i].location.latitude,jsonArray[i].location.longitude),
       map: map,
@@ -58,12 +61,12 @@ function processApi(allText, lat, lng) {
       }
     })(marker, i));
   }
+  pieChart();
 }
 
 function makePage() {
   // getCrimes("52.629729", "-1.131592", "2014-05");
   getLocation();
-  pieChart();
 }
 
 function getCrimes(lat, lng, date) {
@@ -75,12 +78,14 @@ function getCrimes(lat, lng, date) {
   });
 }
 
-function crimeCounter(){
-    var counter = {};
-    counter["burglary"] = 0;
-    return counter;
-};
-
+function addCrime(crimeName) {
+    if (crimes.hasOwnProperty(crimeName)) {
+        crimes[crimeName] = crimes[crimeName] + 1;
+    }
+    else {
+        crimes[crimeName] = 1;
+    }
+}
 function makeImages(){
   var images = {};
   images["all-crime"] = 'images/Zombie_1.png';
@@ -104,31 +109,14 @@ function makeImages(){
   return images;
 }
 function pieChart() {
-    var datah = [
-        {
-            value: 10,
-            color:"#F7464A",
-            highlight: "#FF5A5E",
-            label: "Shoplifting"
-        },
-        {
-            value: 30,
-            color: "#46BFBD",
-            highlight: "#5AD3D1",
-            label: "Bike Theft"
-        },
-        {
-            value: 20,
-            color: "#FDB45C",
-            highlight: "#FFC870",
-            label: "Anti-Social Behaviour Order"
-        }
-    ]
-
-
-    // Get context with jQuery - using jQuery's .get() method.
+    var slices = [];
+    Object.keys(crimes).forEach(function (key) {
+        var slice = [];
+        slice['value'] = crimes[key];
+        slice['color'] = getNextColour();
+        slice['label'] = key;
+        slices.push(slice);
+    })
     var ctx = $("#myChart").get(0).getContext("2d");
-    // This will get the first returned node in the jQuery collection.
-    //var myNewChart = new Chart(ctx);
-    var myDoughnutChart = new Chart(ctx).Doughnut(datah);
+    var myDoughnutChart = new Chart(ctx).Doughnut(slices);
 }
